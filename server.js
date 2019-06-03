69a5e0f7d95ab464fa2af7925feb69cbbd1ab794
coriagogo <pkcparis@gmail.com>
2019-06-03 16:39:08 -0400

    set up server

--- server.js
+++ server.js
@@ -4,4 +4,17 @@ const server = express();
 
 // your code here
 
+server.use(express.json());
+server.use(logger);
+
+server.get('/', (req, res) => {
+    res.send('<h2>Welcome to the DB</h2>');
+})
+
+function logger(req, res, next) {
+    const time = new Date();
+    console.log(`${req.method} ${req.url} ${time}`)
+    next();
+}
+
 module.exports = server;
